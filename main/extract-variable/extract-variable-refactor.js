3a66ab860e55dc6cb8329d46925160550257e3f0
Manh Nguyen <manhnd9293@gmail.com>
2021-01-08 00:24:28 +0700

    update extract variable

--- main/extract-variable/extract-variable-refactor.js
+++ main/extract-variable/extract-variable-refactor.js
@@ -1,5 +1,5 @@
 function price(order) {
-//price is base price ­ quantity discount + shipping
+   //price is base price ­ quantity discount + shipping
     let basePrice = order.quantity * order.itemPrice;
     let discount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
     let shippingFee = Math.min(basePrice * 0.1, 100);
