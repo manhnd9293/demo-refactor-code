c596a555a6dd6093d4e6a9410b92b8e770297566
Nguyen Duc Manh <manhnd@rabiloo.com.vn>
2021-01-07 17:28:22 +0700

    add demo extract variable

---
+++ main/extract-variable/extract-variable-refactor.js
@@ -0,0 +1,7 @@
+function price(order) {
+//price is base price ­ quantity discount + shipping
+    let basePrice = order.quantity * order.itemPrice;
+    let discount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+    let shippingFee = Math.min(basePrice * 0.1, 100);
+    return basePrice - discount + shippingFee;
+}
